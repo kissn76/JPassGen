385ddc8c8196551a3bedd29e3e31975dd65d8f69
Kiss Norbert <nn@desktop1.lan>
2018-10-02 15:43:12 +0200

    JavaFX giu development

--- src/gui/JPassGenGui.java
+++ src/gui/JPassGenGui.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class JPassGenGui extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("jpassgenpanel.fxml"));
